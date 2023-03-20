122181d3c58d43624fdf7483031457863bfc1c3c
AutumnLee172 <78860600+AutumnLee172@users.noreply.github.com>
2023-03-20 15:27:43 +0800

    GUI setup

--- src/module-info.java
+++ src/module-info.java
@@ -1,9 +1,11 @@
-module BCD_Assignment_Autumn {
+module BCD_Assignment_Autumn  {
 	exports bcd.hashing;
 	exports bcd.tester;
+	exports bcd.views;
 	exports bcd.blockchain;
 
-	requires java.sql;
 	requires gson;
+	requires java.desktop;
+	requires java.sql;
 	requires org.apache.commons.codec;
 }
